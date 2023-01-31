f2b65517a0f6b0c2eb7d40938a6efdcbbd991878
Maxim <frodo15895379@gmail.com>
2023-02-01 02:17:58 +0300

    slider, sale

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+    $('.banner-section__slider').slick({
+        dots: true,
+        // Замена стандартных стрелок 
 
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprew"><img src="images/icons/arrow-left.svg" alt="left"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg" alt="right"></button>'
+        
+        // Замена стандартных стрелок 
+    });
 });
