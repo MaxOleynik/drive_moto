75ac2b67c47900ac46927bb6ba9b2ef075fc39b7
Maxim <frodo15895379@gmail.com>
2023-02-08 16:22:43 +0300

    tabs, categories

--- js/main.js
+++ js/main.js
@@ -8,4 +8,17 @@ $(function(){
         
         // Замена стандартных стрелок 
     });
+
+    // табы, переключение
+    $('.search__tabs-item').on('click', function(e){
+        e.preventDefault();
+
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+        $(this).addClass('search__tabs-item--active');
+        $($(this).attr('href')).addClass('search__content-item--active');
+
+        
+    }); 
+    // табы, переключение
 });
